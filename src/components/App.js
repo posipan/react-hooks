f9f47241386aa42bd3ded196ea3858be90a30855
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-01-22 11:44:19 +0900

    solve-prop-drilling-issue

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,14 @@ import { EventForm } from './EventForm';
 import { Events } from './Events';
 import { AppContext } from '../contexts/AppContext';
 
-console.log({ AppContext });
-
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <AppContext.Provider value={'Hello i m provider'}>
+    <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
-        <EventForm state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventForm />
+        <Events />
       </div>
     </AppContext.Provider>
   );
