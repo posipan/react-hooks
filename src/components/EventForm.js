c797f78ed20fcd77ebfc0bae53375372866a8cf4
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-01-22 10:50:40 +0900

    replace type names with constants

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
 
 export const EventForm = ({ state, dispatch }) => {
   const [title, setTitle] = useState('');
@@ -7,7 +8,7 @@ export const EventForm = ({ state, dispatch }) => {
   const addEvent = e => {
     e.preventDefault();
     dispatch({
-      type: 'CREATE_EVENT',
+      type: CREATE_EVENT,
       title,
       body,
     });
@@ -19,7 +20,7 @@ export const EventForm = ({ state, dispatch }) => {
   const deleteAllEvents = e => {
     e.preventDefault();
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？');
-    if (result) dispatch({ type: 'DELTE_ALL_EVENT' });
+    if (result) dispatch({ type: DELETE_ALL_EVENTS });
   }
 
   const unCreatable = title === '' || body === '';
