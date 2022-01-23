0ed016d4ba63f5335d52beada7340f946beed0b9
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-01-23 10:05:59 +0900

    Create operationLogs reducer

---
+++ src/reducers/operationLogs.js
@@ -0,0 +1,18 @@
+import { ADD_OPERRATION_LOG, DELETE_ALL_OPERRATION_LOGS } from "../actions";
+
+const operationLogs = (state = [], action) => {
+  switch (action.type) {
+    case ADD_OPERRATION_LOG:
+      const operationLog = {
+        description: action.description,
+        operatedAt: action.operatedAt,
+      }
+      return [operationLog, ...state];
+    case DELETE_ALL_OPERRATION_LOGS:
+      return [];
+    default:
+      return state;
+  }
+}
+
+export default operationLogs;
